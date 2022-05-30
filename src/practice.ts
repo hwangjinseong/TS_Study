442ea8870b3db36745c6529a13884f27da6d67d9
hwangjinseong <imhjs2023@gmail.com>
2022-05-30 10:04:46 +0900

    일반 객체를 interface 로 타입 설정하기

--- src/practice.ts
+++ src/practice.ts
@@ -42,36 +42,68 @@ function returnNothing(): void {
 // Shape 라는 interface 를 선언합니다.
 interface Shape {
     getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자입니다.
-}
-
-class Circle implements Shape {
+  }
+  
+  class Circle implements Shape {
     // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시합니다.
-    radius: number; // 멤버 변수 radius 값을 설정합니다.
-
-    constructor(radius: number) {
-        this.radius = radius;
+    constructor(public radius: number) {
+      this.radius = radius;
     }
-
-    // 너비 가져오는 함수 구현
+  
+    // 너비를 가져오는 함수를 구현합니다.
     getArea() {
-        return this.radius * this.radius * Math.PI;
+      return this.radius * this.radius * Math.PI;
     }
-}
-
-class Rectangle implements Shape {
-    width: number;
-    height: number;
-    constructor(width: number, height: number) {
-        this.width = width;
-        this.height = height;
+  }
+  
+  class Rectangle implements Shape {
+    constructor(private width: number, private height: number) {
+      this.width = width;
+      this.height = height;
     }
     getArea() {
-        return this.width * this.height;
+      return this.width * this.height;
     }
-}
-
-const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)]
-
-shapes.forEach(shape => {
+  }
+  
+  const circle = new Circle(5);
+  const rectangle = new Rectangle(10, 5);
+  
+  console.log(circle.radius);
+  console.log(rectangle.width); //오류
+  // public으로 선언된 값은 클래스 외부에서 조회 할 수 있으며 private으로 선언된 값은 클래스 내부에서만 조회 할 수 있습니다. 
+  // 따라서 위 코드에서는 circle 의 radius 값은 클래스 외부에서 조회 할 수 있지만, rectangle 의 width 또는 height 값은 클래스 외부에서 조회 할 수 없습니다.
+  
+  const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+  
+  shapes.forEach(shape => {
     console.log(shape.getArea());
-});
+  });
+
+
+// 일반 객체를 interface 로 타입 설정하기
+interface Person {
+    name: string;
+    age?: number;  // 물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값이라는 것을 의미합니다.
+}
+
+interface Developer {
+    name: string;
+    age?: number;
+    skills: string[];
+}
+
+const person: Person = {
+    name: '황진성',
+    age: 18
+};
+
+const expert: Developer = {
+    name: '황개발',
+    skills: ['javascript', 'react']
+};
+// 지금 보면 Person 과 Developer 가 형태가 유사하다
+// 이럴 땐 interface 를 선언 할 때 다른 interface 를 extends 키워드를 사용해서 상속받을 수 있습니다.
+const people: Person[] = [person, expert];
+
+
